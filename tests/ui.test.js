97080e1c34fac72246fdbc5e966f4ca07d5c1e9b
kalinamomkova <kalinamomkova@gmail.com>
2024-03-18 12:20:16 +0200

    add some unit tests

--- tests/ui.test.js
+++ tests/ui.test.js
@@ -1,2 +1,39 @@
 const { test, expect } = require('@playwright/test');
+const baseUrl = 'http://localhost:3000/';
+test("verify all books link is visible", async ({ page }) => {
+    await page.goto(baseUrl);
+    await page.waitForSelector('nav.navbar');
+    const allBooksLink = await page.$('a[href="/catalog"]');
+    const isVisible = await allBooksLink.isVisible();
+    expect(isVisible).toBe(true);
+});
 
+test('verify login button is visible', async ({page}) =>{
+    await page.goto(baseUrl);
+    await page.waitForSelector('nav.navbar');
+    const loginButton = await page.$('a[href="/login"]');
+    const isVisible = await loginButton.isVisible();
+    expect(isVisible).toBe(true);
+});
+
+test('verify register button is visible', async ({page}) =>{
+    await page.goto(baseUrl);
+    await page.waitForSelector('nav.navbar');
+    const registerButton = await page.$('a[href="/register"]');
+    const isVisible = await registerButton.isVisible();
+    expect(isVisible).toBe(true);
+});
+
+test("verify all books link is visible after user is logged in", async ({ page }) => {
+    await page.goto(baseUrl);
+    await page.waitForSelector('nav.navbar');
+
+    await page.click('a[href="/login"]');
+    await page.fill('#email', 'peter@abv.bg');
+    await page.fill('#password', '123456');
+    await page.click('#login-form .button.submit');
+
+    const allBooksLink = await page.$('a[id=logoutBtn]');
+    const isVisible = await allBooksLink.isVisible();
+    expect(isVisible).toBe(true);
+});
